666cb87231ae0e453f9c9959881d4a056eb1c9b5
Naitik Patidar <naitikpatidar090@gmail.com>
2026-05-21 14:40:45 +0530

    Add files via upload

---
+++ Tensors.py
@@ -0,0 +1,23 @@
+import numpy as np 
+
+# 0D Tensor ##
+
+a = np.array(4)
+print(a)
+
+print(a.ndim)
+
+## 1D Tensor ##
+
+arr = np.array([1,2,3,4,5])
+
+print(arr)
+print(arr.ndim)
+
+
+## 2D Tensor/Matrices ##
+
+mat = np.array([[1,2,3], [4,5,6], [7,8,9]])
+print(mat)
+print(mat.ndim)
+
